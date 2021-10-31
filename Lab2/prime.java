d21d3e66135287cfb059c3ab31d455f2c5141df0
Chethan <86680859+ChethanKumar-K@users.noreply.github.com>
2021-10-31 13:16:36 +0530

    Update prime.java

--- Lab2/prime.java
+++ Lab2/prime.java
@@ -1,26 +1,22 @@
-import java.lang.*;
 import java.util.Scanner;
-
-//Print the prime numbers between two integers
-
-class Prime{
-	public static void main(String args[]){
-		int a,b,flag=0,j,k=2;
-		Scanner x=new Scanner(System.in);
-		System.out.print("Enter the first number :");
+public class Main
+{
+	public static void main(String[] args) {
+	    int a,b;
+	    String prime="The prime nos are: ";
+	    int flag=0;
+	    Scanner x=new Scanner(System.in);
+		System.out.print("Enter the number a:");
 		a=x.nextInt();
-		System.out.print("Enter the second number :");
+		System.out.print("Enter the number b:");
 		b=x.nextInt();
 		for(int i=a;i<=b;i++){
-			while(k<=i/2){
-				if(i%k==0){
-					flag=1;break;
-				}
-				k++;
-			}
-			if(flag==0){
-				System.out.print(i+" ");
-			}
-		}
+		    flag=0;
+		    for(int j=2;j<=i/2;j++){
+		        if(i%j==0) flag=1;
+		    }
+		    if(flag == 0) prime = prime+i+" ";
+	    }
+	    System.out.println(prime);
 	}
 }
