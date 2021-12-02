e8c95a5102fac15f902f09cc92ae0af1c0e42a5c
Chethan <86680859+ChethanKumar-K@users.noreply.github.com>
2021-12-03 05:04:03 +0530

    Update toString.java

--- Lab3/toString.java
+++ Lab3/toString.java
@@ -11,7 +11,7 @@ class Book{
         num_pages=np;
     }
     public String toString(){
-        return ("\nname="+name+"\nauthor : "+author+"\nprice : "+price+"\nnum_pages : "+num_pages+"\n");
+        return ("\nname :"+name+"\nauthor : "+author+"\nprice : "+price+"\nnum_pages : "+num_pages+"\n");
     }
 }
 class Main{
@@ -23,14 +23,23 @@ class Main{
         int j=x.nextInt();
         Book[] a=new Book[j];
         for(int i=0;i<j;i++){
+            System.out.println("Enter the name of the book "+(i+1)+":");
             nam=x.nextLine();
+            System.out.println("Enter the author of the book "+(i+1)+":");
             aut=x.nextLine();
+            System.out.println("Enter the price of the book "+(i+1)+":");
             pri=x.nextInt();
+            System.out.println("Enter the number of pages of the book "+(i+1)+":");
             nump=x.nextInt();
             a[i] =new Book(nam,aut,pri,nump);
         }
         System.out.print("Enter the index of the book array whose details is to be displayed : ");
         int b=x.nextInt();
-        System.out.print(toString(a[b]));
+        if(b>=0 && b<=j){
+            System.out.print(a[b]);
+        }
+        else{
+            System.out.println("No such index exist");
+        }
     }
 }
